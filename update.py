370c8d6c9aa37e2caa297b0586705a223d53496b
Dark.Hades <dark.hades.1102@gmail.com>
2017-09-14 21:50:25 +0900

    update

--- update.py
+++ update.py
@@ -1,6 +1,21 @@
+#!/usr/bin/env python
+
 import os
+import sys
 import subprocess
 
+print('Update Pathogen', '\n')
+subprocess.call([
+  'curl', '-LSo',
+  os.path.dirname(os.path.abspath(__file__)) + '/.vim/autoload/pathogen.vim',
+  'https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim'
+])
+print('Finished', '\n')
+
+print('Submodule', '\n')
 subprocess.call(['git', 'pull', 'origin', 'master'])
 subprocess.call(['git', 'submodule', 'update', '--recursive', '--remote'])
 subprocess.call(['git', 'submodule', 'update', '--recursive', '--init'])
+
+print('Rebase', '\n')
+subprocess.call(['git', 'pull', '--rebase'])
