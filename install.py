6d2eaf06a6723b41e4318ef10724ee85bd00e3de
HaiLe <dark.hades.1102@gmail.com>
2017-09-27 15:09:05 +0900

    Script update

--- install.py
+++ install.py
@@ -7,14 +7,36 @@ import shutil
 DIR = os.path.dirname(os.path.realpath(__file__))
 CWD = os.getcwd()
 
-print('Symlink')
-if sys.platform == "linux2" or sys.platform == "darwin":
-  # Is Linux
+if (sys.platform == 'linux2') or (sys.platform == 'darwin'):
+  print('Unix :: found')
+
   HOME = os.environ['HOME']
-  shutil.rmtree(HOME + '/.vim')
-  os.remove(HOME + '/.vimrc')
-  os.symlink(DIR + '/.vim', HOME + '/.vim')
-  os.symlink(DIR + '/.vim/.vimrc', HOME + '/.vimrc')
-elif sys.platform == "win32":
+
+  print('Vim :: start :: Delete ~/.vim')
+  dot_vim_dir = HOME + '/.vim'
+  if (os.path.exists(dot_vim_dir)):
+    if (os.path.islink(dot_vim_dir)):
+      os.remove(dot_vim_dir)
+    else:
+      shutil.rmtree(dot_vim_dir)
+
+  print('Vim :: start :: Delete ~/.vimrc')
+  dot_vimrc_file = HOME + '/.vimrc'
+  if (os.path.exists(dot_vimrc_file)):
+    os.remove(dot_vimrc_file)
+
+  print('Vim :: start :: Symlink')
+  os.symlink(DIR + '/.vim', dot_vim_dir)
+  os.symlink(DIR + '/.vim/.vimrc', dot_vimrc_file)
+elif (sys.platform == "win32"):
   # Is Windows
   print('Updating...')
+else:
+  print('OS not found')
+
+import update
+
+update.renewPathogen()
+update.gitSubmodule()
+update.gitRebase()
+update.tmux()
--- update.py
+++ update.py
@@ -4,21 +4,30 @@ import os
 import sys
 import subprocess
 
-print('Update Pathogen', '\n')
-subprocess.call([
-  'curl', '-LSo',
-  os.path.dirname(os.path.abspath(__file__)) + '/.vim/autoload/pathogen.vim',
-  'https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim'
-])
-print('Finished', '\n')
+def renewPathogen():
+  print('Update Pathogen', '\n')
+  subprocess.call([
+    'curl', '-LSo',
+    os.path.dirname(os.path.abspath(__file__)) + '/.vim/autoload/pathogen.vim',
+    'https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim'
+  ])
+  print('Finished', '\n')
 
-print('Submodule', '\n')
-subprocess.call(['git', 'pull', 'origin', 'master'])
-subprocess.call(['git', 'submodule', 'update', '--recursive', '--remote'])
-subprocess.call(['git', 'submodule', 'update', '--recursive', '--init'])
+def gitSubmodule():
+  print('Submodule', '\n')
+  subprocess.call(['git', 'pull', 'origin', 'master'])
+  subprocess.call(['git', 'submodule', 'update', '--recursive', '--remote'])
+  subprocess.call(['git', 'submodule', 'update', '--recursive', '--init'])
 
-print('Rebase', '\n')
-subprocess.call(['git', 'pull', '--rebase'])
+def gitRebase():
+  print('Rebase', '\n')
+  subprocess.call(['git', 'pull', '--rebase'])
 
-print('Tmux', '\n')
-subprocess.call(['tmux', 'source', os.path.dirname(os.path.abspath(__file__)) + '/.tmux/.tmux.conf'])
+def tmux():
+  print('Tmux', '\n')
+  subprocess.call(['tmux', 'source', os.path.dirname(os.path.abspath(__file__)) + '/.tmux/.tmux.conf'])
+
+renewPathogen()
+gitSubmodule()
+gitRebase()
+tmux()
