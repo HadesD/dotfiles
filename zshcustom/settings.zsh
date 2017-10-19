fd6203ea92da2c669477bd2e2d1d799564b2bf37
Hai Le <levanhai@u-shop.co.jp>
2017-10-19 17:05:47 +0900

    Config zsh

--- zshcustom/settings.zsh
+++ zshcustom/settings.zsh
@@ -5,5 +5,8 @@ export ZSH_THEME="candy"
 
 alias vim="stty stop '' -ixoff ; vim"
 alias vi=vim
+alias tmux="TERM=xterm-256color tmux"
+export EDITOR=vim
+
 # # `Frozing' tty, so after any command terminal settings will be restored
 ttyctl -f
