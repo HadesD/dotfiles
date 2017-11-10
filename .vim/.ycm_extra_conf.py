28682f57e730ffd45a727a5f5b4019d4c2b02782
Dark.Hades <dark.hades.1102@gmail.com>
2017-11-10 21:54:39 +0900

    Cocos project

--- .vim/.ycm_extra_conf.py
+++ .vim/.ycm_extra_conf.py
@@ -37,44 +37,44 @@ import ycm_core
 # compilation database set (by default, one is not set).
 # CHANGE THIS LIST OF FLAGS. YES, THIS IS THE DROID YOU HAVE BEEN LOOKING FOR.
 flags = [
-'-Wall',
-'-Wextra',
-'-Werror',
-'-Wno-long-long',
-'-Wno-variadic-macros',
-'-fexceptions',
-'-DNDEBUG',
-# You 100% do NOT need -DUSE_CLANG_COMPLETER in your flags; only the YCM
-# source code needs it.
-'-DUSE_CLANG_COMPLETER',
-# THIS IS IMPORTANT! Without the '-x' flag, Clang won't know which language to
-# use when compiling headers. So it will guess. Badly. So C++ headers will be
-# compiled as C headers. You don't want that so ALWAYS specify the '-x' flag.
-# For a C project, you would set this to 'c' instead of 'c++'.
-'-x',
-'c++',
-'-isystem',
-'../BoostParts',
-'-isystem',
-get_python_inc(),
-'-isystem',
-'../llvm/include',
-'-isystem',
-'../llvm/tools/clang/include',
-'-I',
-'.',
-'-I',
-'./ClangCompleter',
-'-isystem',
-'./tests/gmock/gtest',
-'-isystem',
-'./tests/gmock/gtest/include',
-'-isystem',
-'./tests/gmock',
-'-isystem',
-'./tests/gmock/include',
-'-isystem',
-'./benchmarks/benchmark/include',
+  '-Wall',
+  '-Wextra',
+  '-Werror',
+  '-Wno-long-long',
+  '-Wno-variadic-macros',
+  '-fexceptions',
+  '-DNDEBUG',
+  # You 100% do NOT need -DUSE_CLANG_COMPLETER in your flags; only the YCM
+  # source code needs it.
+  '-DUSE_CLANG_COMPLETER',
+  # THIS IS IMPORTANT! Without the '-x' flag, Clang won't know which language to
+  # use when compiling headers. So it will guess. Badly. So C++ headers will be
+  # compiled as C headers. You don't want that so ALWAYS specify the '-x' flag.
+  # For a C project, you would set this to 'c' instead of 'c++'.
+  '-x',
+  'c++',
+  '-isystem',
+  '../BoostParts',
+  '-isystem',
+  get_python_inc(),
+  '-isystem',
+  '../llvm/include',
+  '-isystem',
+  '../llvm/tools/clang/include',
+  '-I',
+  '.',
+  '-I',
+  './ClangCompleter',
+  '-isystem',
+  './tests/gmock/gtest',
+  '-isystem',
+  './tests/gmock/gtest/include',
+  '-isystem',
+  './tests/gmock',
+  '-isystem',
+  './tests/gmock/include',
+  '-isystem',
+  './benchmarks/benchmark/include',
 ]
 
 # Clang automatically sets the '-std=' flag to 'c++14' for MSVC 2015 or later,
@@ -83,14 +83,40 @@ get_python_inc(),
 if platform.system() != 'Windows':
   flags.append( '-std=c++11' )
 
+#################################################################################
 # CaroOnline
+#################################################################################
 if (os.path.exists('Common/third_party/asio')):
-  flags.append( '-I' )
-  flags.append( '../Common/third_party/asio/asio/include' )
+  flags.append(['-I', '../Common/third_party/asio/asio/include'])
+  if (os.path.exists('Common/third_party/asio')):
+    flags.append(['-I', '../Common/third_party/spdlog/include'])
 
-if (os.path.exists('Common/third_party/asio')):
-  flags.append( '-I' )
-  flags.append( '../Common/third_party/spdlog/include' )
+#################################################################################
+#                                   Cocos Dev                                   #
+#################################################################################
+if (os.path.exists('cocos2d')):
+  flags.append(['-I', 'cocos2d'])
+  flags.append(['-I', 'cocos2d/cocos'])
+  flags.append(['-I', 'cocos2d/cocos/base'])
+  flags.append(['-I', 'cocos2d/cocos/physics'])
+  flags.append(['-I', 'cocos2d/cocos/math'])
+  flags.append(['-I', 'cocos2d/cocos/2d'])
+  flags.append(['-I', 'cocos2d/cocos/ui'])
+  flags.append(['-I', 'cocos2d/cocos/network'])
+  flags.append(['-I', 'cocos2d/cocos/audio/include'])
+  flags.append(['-I', 'cocos2d/cocos/editor-support'])
+  flags.append(['-I', 'cocos2d/extensions'])
+  flags.append(['-I', 'cocos2d/external'])
+  flags.append(['-I', 'cocos2d/external/chipmunk/include/chipmunk'])
+
+  if (sys.platform == "darwin"):
+    flags.append(['-I', 'cocos2d/external/curl/include/ios'])
+    flags.append(['-I', 'cocos2d/cocos/platform/mac'])
+    flags.append(['-I', 'cocos2d/cocos/platform/ios'])
+  # elif (sys.platform == "linux" or sys.platform == "linux2"):
+    # Linux
+  # elif (sys.platform == "win32"):
+    # Windows
 
 # Set this to the absolute path to the folder (NOT the file!) containing the
 # compile_commands.json file to use that instead of 'flags'. See here for
