6d41b9d7713a4c6204cfd323c2e3b93342fd3c4a
Hai Le <levanhai@u-shop.co.jp>
2018-01-15 14:45:42 +0900

    lss command

--- zshcustom/settings.zsh
+++ zshcustom/settings.zsh
@@ -7,6 +7,7 @@ alias vim="stty stop '' -ixoff ; vim"
 alias vi=vim
 alias tmux="TERM=xterm-256color tmux"
 export EDITOR=vim
+alias lss='ls -la --block-size=M'
 
 # # `Frozing' tty, so after any command terminal settings will be restored
 ttyctl -f
