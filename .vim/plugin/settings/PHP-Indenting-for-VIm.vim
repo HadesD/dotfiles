c4a9efd6ba45485f7db4c2b5a99a6cd2d719cb0a
Hai Le <levanhai@u-shop.co.jp>
2018-03-05 12:19:42 +0900

    Add color plugin

---
+++ .vim/plugin/settings/PHP-Indenting-for-VIm.vim
@@ -0,0 +1,5 @@
+let g:PHP_outdentphpescape = 0
+let g:PHP_removeCRwhenUnix = 1
+let g:PHP_BracesAtCodeLevel = 0
+let g:PHP_vintage_case_default_indent = 1
+
--- .vim/tests/test.php
+++ .vim/tests/test.php
@@ -1,5 +1,4 @@
 <?php
-
 $f = [
   '' => '',
 ];
@@ -8,10 +7,26 @@ $b = '123123';
 
 function c()
 {
+  switch ($b)
+  {
+    case 1:
+      c();
+      break;
+    case 2:
+      f();
+      break;
+    default:
+      ff();
+  }
 
+  if (1 === 2)
+  {
+    echo 2;
+  }
+  elseif (1 !== 2)
+  {
+    echo 1;
+  }
 }
 
 for ($i = 0;$i < 10;$i++);
-
-fff
-
