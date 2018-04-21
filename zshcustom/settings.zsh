a1592ff9acb0d70c291fae831d8dad92392fd863
Dark.Hades <dark.hades.1102@gmail.com>
2018-04-21 22:41:34 +0900

    Fix error shown

--- zshcustom/settings.zsh
+++ zshcustom/settings.zsh
@@ -18,7 +18,7 @@ export PATH=$HOME/.local/usr/bin:$PATH
 
 # Run if found ibus
 IBUS_RUNNING=$(ps aux | grep ibus-daemon | grep -v "grep" | awk '{print $2}')
-if [[ -f "$(which ibus-daemon)" ]]; then
+if type "ibus-daemon" > /dev/null; then
   if [[ -z $IBUS_RUNNING ]]; then
     ibus-daemon -drx
   fi
