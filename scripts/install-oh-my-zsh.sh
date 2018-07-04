3d182bf7b647c962b41fb3270cc64b4cb08472ec
Dark.Hades <dark.hades.1102@gmail.com>
2018-07-04 23:01:50 +0900

    Update libs

--- scripts/install-oh-my-zsh.sh
+++ scripts/install-oh-my-zsh.sh
@@ -1,1 +1,2 @@
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
