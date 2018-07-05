7d0ffc04c0fa36f8b85d0908bf9015eb3aed10d6
Dark.Hades <dark.hades.1102@gmail.com>
2018-07-05 17:35:55 +0900

    Fix vimrc

--- .vim/after/ftplugin/cpp.vim
+++ .vim/after/ftplugin/cpp.vim
@@ -1,3 +1,6 @@
-" setlocal cindent cino=j1,(0,ws,Ws
-setlocal cindent cino=g2,+0,j1,(0,ws,Ws
+setlocal cindent
+setlocal cino=g2,+0,j1,(0,ws,Ws
+
+" handle lambda correctly
+" setlocal cino=j1,(0,ws,Ws
 
--- .vim/tests/test.cpp
+++ .vim/tests/test.cpp
@@ -1,32 +1,54 @@
+#include <functional>
 #include <iostream>
+#include <map>
 #include <string>
 #include <thread>
+#include <tuple>
 #include <vector>
-#include <map>
+#include <future>
 
-namespace test::names
-{
-  template<typename T>
-  class TestClass
-  {
+namespace test::names {
+  template <typename T>
+  class TestClass {
     public:
       TestClass();
       ~TestClass();
-    public:
-      void testCase(const std::string& t)
-      {
-      }
 
-      template<typename T>
-      int d()
-      {
+    public:
+      void testCase(const std::string &t);
+
+      template <typename T1>
+      int d() {
         T a;
 
         return a;
       }
-
   };
-}
+
+  template <typename T, typename Arg, typename... Args>
+  class TestIndent {
+    private:
+      int a = 1;
+
+    private:
+      std::function<std::string(const std::vector<std::string> &)> f;
+      std::tuple<std::string, int> t;
+      std::tuple<
+      int,
+      char,
+      float,
+      bool, double,
+      std::string,
+      std::vector<
+      std::string
+      >,
+      std::tuple<int, float, bool>
+      > t2;
+
+    public:
+      TestIndent();
+  };
+} // namespace test::names
 
 // sdfsdf
 /**
@@ -51,10 +73,9 @@ enum class TestEnum
   TEST,
 };
 
-class Test
-{
+class Test {
   public:
-    template<typename S>
+    template <typename S>
     struct FF : public S
     {
     };
@@ -62,26 +83,31 @@ class Test
 
 int f = 1;
 
-int main()
-{
+int main() {
   int i;
 
   i = 1;
 
-  std::thread f([](){
+  std::thread f([]() {
     int i = 1;
-    for (; i < 10; i++)
-    {
+    for (; i < 10; i++) {
       int f;
       f = 3;
     }
   });
+
+  std::async(
+   std::launch::async,
+   [](){
+     int a = 1;
+   }
+   );
 
   std::map<std::pair<int, int>, int> m;
   m[{1, 2}] = 3;
 
   std::vector<std::string> v();
 
-  std::vector<std::string> v3 = { "f", "", "", "" };
+  std::vector<std::string> v3 = {"f", "", "", ""};
 }
 
