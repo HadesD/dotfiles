2c1899b562e1501bb1c11ca269e080b4b8798c7e
Dark.Hades <dark.hades.1102@gmail.com>
2018-07-09 09:33:23 +0900

    Fix script

--- scripts/install-oh-my-zsh.sh
+++ scripts/install-oh-my-zsh.sh
@@ -1,2 +1,6 @@
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+if [ $(type curl >/dev/null 2>&1) ]; then
+  sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+else
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+fi
 
