d1ec5bcb4e1f64d1681f8d4737c6b68686b2c93b
Dark.Hades <dark.hades.1102@gmail.com>
2018-07-15 22:08:27 +0900

    Update lib

--- scripts/install-oh-my-zsh.sh
+++ scripts/install-oh-my-zsh.sh
@@ -1,6 +1,6 @@
 if [ $(type curl >/dev/null 2>&1) ]; then
-  sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-else
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+else
+  sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 fi
 
