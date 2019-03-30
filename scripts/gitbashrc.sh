336b64ea6a56884713cebd284e25bed30f1c4567
Dark.Hades <dark.hades.1102@gmail.com>
2019-03-30 22:33:55 +0900

    Update gitbashrc

--- scripts/gitbashrc.sh
+++ scripts/gitbashrc.sh
@@ -1,16 +1,31 @@
+#!/usr/bin/env bash
+
 ###############################################################################
 # GitBash.exe
 # vi ~/.bashrc
-# source /d/ProgramFiles/scripts/gitbashrc.sh
+# source /d/Documents/Git/dotfiles/scripts/gitbashrc.sh
 ###############################################################################
 
 #source /d/Documents/Git/emsdk/emsdk_env.sh
 
-export PATH=$PATH:/d/ProgramFiles/cmake-3.12.2-win32-x86/bin
+# export PATH=$PATH
+
+_PATH_LIST=(
+/d/ProgramFiles/cmake-3.12.2-win32-x86/bin
+/d/ProgramFiles/msys2-bin
 #:/d/Programming/docker
+)
+_TEMP_PATH=$PATH
+for i in "${_PATH_LIST[@]}"; do
+  if [ -d $i ]; then
+    _TEMP_PATH=$_TEMP_PATH:$i
+  fi
+done
+export PATH=$_TEMP_PATH
 
 # Check docker-machine
 which docker-machine > /dev/null 2>&1
-if [ $? -eq 0 ]; then 
+if [ $? -eq 0 ]; then
 	eval $(docker-machine env default)
 fi
+
