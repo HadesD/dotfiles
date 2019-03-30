a6b25587001ff64adb3170db85d8f5bced65af89
Dark.Hades <dark.hades.1102@gmail.com>
2019-03-31 00:47:11 +0900

    Bash

--- bashcustom/bashrc.sh
+++ bashcustom/bashrc.sh
@@ -1,4 +1,21 @@
+#!/usr/bin/env bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+export LC_ALL=en_US.UTF-8
+export EDITOR=vim
+
 if [ -z "$TMUX"  ]; then
-  export TERM='xterm-256color'
+  export TERM=xterm-256color
+fi
+
+# # `Frozing' tty, so after any command terminal settings will be restored
+export GIT_LFS_SKIP_SMUDGE=1
+
+alias vi=vim
+alias lss='ls -la --block-size=h'
+
+if [ -f ${DIR}/_os/${OSTYPE}.sh ]; then
+  source ${DIR}/_os/${OSTYPE}.sh
 fi
 
