fe3f99a15a06988dec79a80754b7a96c4ae4ce71
Dark.Hades <dark.hades.1102@gmail.com>
2019-03-31 01:04:51 +0900

    Use bash-it

---
+++ scripts/install-bash-it.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+SCRIPT_NAME=$0
+PWD=$(pwd)
+DIR=${PWD}/$(dirname ${SCRIPT_NAME})
+
+git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
+~/.bash_it/install.sh
+
+sed -i "1s;^;export BASH_IT_CUSTOM=${DIR}\/..\/bashcustom\n;" ~/.bashrc
+
