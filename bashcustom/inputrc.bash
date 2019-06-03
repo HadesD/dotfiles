1469f0416011e4fd09ff0944c9e333a02c73a59b
Dark.Hades <dark.hades.1102@gmail.com>
2019-06-03 14:45:11 +0700

    Update

--- bashcustom/inputrc.bash
+++ bashcustom/inputrc.bash
@@ -2,3 +2,7 @@ bind "TAB:menu-complete"
 bind "set show-all-if-ambiguous on"
 bind "set menu-complete-display-prefix on"
 bind "set completion-ignore-case on"
+bind '"\e[A": history-search-backward'
+bind '"\eOA": history-search-backward'
+bind '"\e[B": history-search-forward'
+bind '"\eOB": history-search-forward'
