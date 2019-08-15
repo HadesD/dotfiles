8e8c31daa0dbaee190aa8c7e7e89b0939ebd82ca
Dark.Hades <dark.hades.1102@gmail.com>
2019-08-15 16:43:43 +0700

    Bind key

---
+++ .local/etc/zshcustom/bindkey.zsh
@@ -0,0 +1,8 @@
+bindkey '\e[1~'   beginning-of-line  # Linux console
+bindkey '\e[H'    beginning-of-line  # xterm
+bindkey '\eOH'    beginning-of-line  # gnome-terminal
+bindkey '\e[2~'   overwrite-mode     # Linux console, xterm, gnome-terminal
+bindkey '\e[3~'   delete-char        # Linux console, xterm, gnome-terminal
+bindkey '\e[4~'   end-of-line        # Linux console
+bindkey '\e[F'    end-of-line        # xterm
+bindkey '\eOF'    end-of-line        # gnome-terminal
