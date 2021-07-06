7c275b81353cf5120e8e28417c978f0d8b6d530b
Ichiro <dark.hades.1102@gmail.com>
2021-07-07 04:24:18 +0900

    Update

--- .local/etc/zshcustom/settings.zsh
+++ .local/etc/zshcustom/settings.zsh
@@ -1,25 +1,27 @@
 export LC_ALL=en_US.UTF-8
-# Theme setting
-# unset ZSH_THEME
-RPROMPT='[%D{%H:%M:%S}]'
-export ZSH_THEME="mortalscumbag"
 
-# alias vim="stty stop '' -ixoff ; vim"
+# Theme setting
+RPROMPT='[%D{%H:%M:%S}]'
+if [[ "$(uname -a)" == *"microsoft"* ]]; then
+  local ret_status="%(?:%{$fg_bold[green]%}:%{$fg_bold[red]%})%?%{$reset_color%}"
+  RPROMPT="${RPROMPT} [${ret_status}]"
+  ZSH_THEME="steeef"
+else
+  ZSH_THEME="mortalscumbag"
+fi
+
 export EDITOR=vim
-export SHELL=$(which zsh)
-if [ -z "$TMUX"  ]; then
+if [ -z "$TMUX" ]; then
   export TERM='xterm-256color'
 fi
 
 alias vi=vim
-alias lss='ls -la --block-size=h'
 
 # # `Frozing' tty, so after any command terminal settings will be restored
 ttyctl -f
 export GIT_LFS_SKIP_SMUDGE=1
 
-export PATH=$HOME/.local/bin:$HOME/.local/usr/bin:$PATH
+export PATH=$PATH:$HOME/.local/bin:$HOME/.local/usr/bin
 
-#so as not to be disturbed by Ctrl-S ctrl-Q in terminals:
+# so as not to be disturbed by Ctrl-S ctrl-Q in terminals:
 stty -ixon
-
