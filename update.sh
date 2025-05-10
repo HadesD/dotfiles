5cddbd0e1473a51c7a78aa5c088a41aa114ef120
ws <ws@local>
2025-05-10 13:48:20 +0900

    Update

--- update.sh
+++ update.sh
@@ -1,9 +1,8 @@
 git pull origin master
-git submodule update --jobs=5 --init --recursive
+git submodule update --jobs=5 --init --recursive --recommend-shallow --depth 1
 
 git submodule foreach "
   git reset HEAD
   git checkout .
   git pull origin \$(basename \$(git symbolic-ref refs/remotes/origin/HEAD --short))
 "
-
